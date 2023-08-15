742c041c3f78ef8f2bf25452b4b26b9ec4e858ac
Jason <jasontorrealba199@gmail.com>
2023-08-15 15:52:16 -0400

    Added GET notes and GET * routes

---
+++ routes/html.js
@@ -0,0 +1,14 @@
+const html = require('express').Router();
+const path = require('.path');
+
+// GET Notes returns notes.html file
+html.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// GET * returns index.html file
+html.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+module.exports = html;
