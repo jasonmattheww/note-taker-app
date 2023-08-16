5fb791a4aa4e390129e71df96b94b07f6d3cf0d5
Jason <jasontorrealba199@gmail.com>
2023-08-16 13:02:57 -0400

    Edited code for returning files

--- routes/html.js
+++ routes/html.js
@@ -2,13 +2,13 @@ const html = require('express').Router();
 const path = require('path');
 
 // GET Notes returns notes.html file
-html.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+html.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // GET * returns index.html file
-html.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+html.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 module.exports = html;
