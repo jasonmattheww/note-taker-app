80a72e48a6db71556cb5cb2d3ff25f7c063a0090
Jason <jasontorrealba199@gmail.com>
2023-08-16 13:56:55 -0400

    Renamed api file

---
+++ routes/apiRoute.js
@@ -0,0 +1,29 @@
+const apiRoute = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+// GET /api/notes reads the db.json file and returns all saved notes as JSON
+apiRoute.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) throw err;
+    let dbData = JSON.parse(data);
+    res.json(dbData)
+  });
+});
+
+// POST /api/notes receives a new note to save on the request body and adds it to db.json, then returns new note to client
+apiRoute.post('/api/notes', (req, res) => {
+  
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+
+  dbData.push(newNote);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(dbData))
+  res.json(newNote);
+});
+
+module.exports = apiRoute;
