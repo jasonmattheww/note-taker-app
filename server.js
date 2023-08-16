398286b4271e7c063383c21a93c3b12fd4a7ca15
Jason <jasontorrealba199@gmail.com>
2023-08-16 14:00:44 -0400

    When back to old code

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoute');
-const htmlRoutes = require('./routes/html')
+
 // Import routes
-// const api = require('./routes/index.js');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,12 +10,10 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
