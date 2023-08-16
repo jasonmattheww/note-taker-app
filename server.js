a945d339d0e5f67bf475678d5c4c3a04bcd6b0c7
Jason <jasontorrealba199@gmail.com>
2023-08-15 21:05:57 -0400

    Edited port for server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // Import routes
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
