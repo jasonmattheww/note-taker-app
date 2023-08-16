2152f8fc64fb34737f81d3828032bcf2831eb26d
Jason <jasontorrealba199@gmail.com>
2023-08-16 15:17:15 -0400

    Changed GET notes line

--- routes/html.js
+++ routes/html.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // GET Notes returns notes.html file
 html.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, '/../routes/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/../public/notes.html'))
 );
 
 // GET * returns index.html file
