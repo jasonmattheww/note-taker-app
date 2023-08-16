5f1741c01727c3dbf3bc19668165c315662d969e
Jason <jasontorrealba199@gmail.com>
2023-08-16 13:39:28 -0400

    Added api and html routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const apiRouter = require('./api');
+const htmlRouter = require('./html');
+
 // Import routes
-const api = require('./routes/index.js');
+// const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,9 +12,12 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
+
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
