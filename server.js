c8fa7959f724948142b7570d134d2d1e8b02d434
Jason <jasontorrealba199@gmail.com>
2023-08-16 13:45:50 -0400

    Delted code and un commented old code

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const apiRouter = require('./api');
-const htmlRouter = require('./html');
 
 // Import routes
-// const api = require('./routes/index.js');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,12 +10,9 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
-
-app.use('/api', apiRouter);
-app.use('/html', htmlRouter);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
