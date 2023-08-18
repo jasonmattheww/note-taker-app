d83db5ad68048e036df14f96216bf2a6ef6643f7
Jason <jasontorrealba199@gmail.com>
2023-08-17 22:36:22 -0400

    Edited GET and POST request and added DELETE request

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,16 +3,17 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 // GET /api/notes reads the db.json file and returns all saved notes as JSON
-apiRoute.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', (err, data) => {
+apiRoute.get('/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    console.log(data);
     if (err) throw err;
     let dbData = JSON.parse(data);
-    res.json(dbData)
+    return res.json(dbData)
   });
 });
 
 // POST /api/notes receives a new note to save on the request body and adds it to db.json, then returns new note to client
-apiRoute.post('/api/notes', (req, res) => {
+apiRoute.post('/notes', (req, res) => {
   fs.readFile('./db/db.json', (err, data) => {
     if (err) throw err;
 
@@ -29,6 +30,15 @@ apiRoute.post('/api/notes', (req, res) => {
   fs.writeFileSync('./db/db.json', JSON.stringify(dbData))
   res.json(newNote);
   });
+});
+
+apiRoute.delete('/notes/:id', (req, res) => {
+  let dbData = JSON.parse(fs.readFileSync('db/db.json'))
+
+  let deleteNotes = dbData.filter(item => item.id !== req.params.id);
+
+  fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
 });
 
 module.exports = apiRoute;
